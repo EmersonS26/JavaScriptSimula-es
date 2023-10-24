b92c315237d0c21981da9d5737ae908f07de90d7
secthor01 <132622930+secthor01@users.noreply.github.com>
2023-10-24 18:38:06 -0300

    ATT Funções_objeto.js

--- Funções_objeto.js
+++ Funções_objeto.js
@@ -1,24 +1,26 @@
-function Carro(nome,marca,ano){
+//ESSE SE CHAMA FACTORY
+function Carro(nome,marca,ano){//função com uma lista orientada a objetos,tema carro
     let car = {
         nomeCarro : nome,
         marcaCarro : marca,
         anoCarro : ano,
-        motor:()=>{
-            console.log("massa")
+        motor:()=>{//função dentro de uma função que dá o comando de exibir: "Carro Moderno",ao invés de usar: function motor(){ ou motor:function(){,eu simplifiquei com uma arrow function(=>) esse sinal é o function,ou seja FUNCTION É = =>
+            console.log("Carro Moderno")
 
         }
             
     }
-    return car
+    return car //retorna o valor de carro(nome,marca,ano)
 }
-const modelo = Carro("X7-EXTREME","BMW",2024)
-console.log(modelo)
+const modelo = Carro("X7-EXTREME","BMW",2024)//criação da variavel que irá exibir o valor dentro da função,puxado pelo return
+console.log(modelo)//exibe a função completa
 
 
-function carro(nome,mas,simples){
+//ESSE SE CHAMA CONSTRUCTOR
+function carro(nome,mas,simples){//esse faz a mesma coisa do primeiro,entretante é mais rápido,prático e simples(mas não tão bem organizando e visual quanto o primeiro)
     this.nome = nome;
     this.simples = simples;
     this.mas = mas;
 }
-const a = new carro("carro","mercedez",2023)
-console.log(a)
+const a = new carro("carro","mercedez",2023)//criação da variavel que irá armazenar os valores a serem exibidos dentro da função
+console.log(a)//exibe a função completa
